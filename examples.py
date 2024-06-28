15323eaaf5fbaa581236fad6156efc906375f93e
EmiliaGibson <87773313+EmiliaGibson@users.noreply.github.com>
2024-06-28 14:20:03 +0100

    Update examples.py

--- examples.py
+++ examples.py
@@ -1,12 +1,11 @@
-#%%
 import numpy as np
 import matplotlib.pyplot as plt
 from utils import find_surfaces
 
-#%%
+
 datasets = ['Curvilinear-Sierpinski-data.npy','Henon-data.npy','Sierpinski-data.npy','Logistic-data.npy']
 
-#
+#dictionary with parameters for each dataset
 dataset_kwargs = {
     'Curvilinear-Sierpinski-data.npy': {
         "delta":0.15,
@@ -26,18 +25,21 @@ dataset_kwargs = {
     }
 }
 
-# choose dataset 0 = curvilinear sierpisnki, 1 = henon or 2 = sierpinski 
+# choose dataset 0 = curvilinear sierpisnki, 1 = henon, 2 = sierpinski, 3 = logistic  
 key = 2
 assert key in [0,1,2,3]
 dataset = datasets[key]
+
+#load dataset
 data = np.load(datasets[key])
 
+#embed the data to higher dimension
 embedded_data = np.column_stack((data[:-1,:],data[1:,0]))
-# a = dataset_kwargs['Henon-data.npy'][key1]
-# b = dataset_kwargs['Henon-data.npy'][key2]
+
+#apply separation algorithm
 surfaces = find_surfaces(embedded_data, **dataset_kwargs[dataset])
 
-#%%
+
 #plot the different surfaces and their respective densities
 fig = plt.figure(figsize=(6,6))
 ax = plt.axes(projection='3d')
@@ -56,26 +58,4 @@ for i in range(len(surfaces)):
 plt.bar(np.arange(1,len(surfaces)+1),distr/np.sum(distr))
 plt.ylabel('density')
 plt.xlabel('$f_i$')
-#%%
-def g(x,j):
-    if j == 0:
-        return 4*x*(1-x)
-    if j == 1:
-        return 3.8*x*(1-x)
-    if j == 2:
-        return 3.6*x*(1-x)
-    else:
-        return ValueError('j != 0,1,2')
-    
-#%%
-np.random.seed(10)
-N = 10000
-A = np.zeros((N,1))
-A[0,:] = np.random.uniform(0,1)
-colours = [-1]
-for i in range(N-1):
-    j = np.random.randint(0,3)
-    colours.append(j)
-    A[i+1, :] = g(A[i,:],j)
-np.save('Logistic-data.npy',A)
-# %%
+
