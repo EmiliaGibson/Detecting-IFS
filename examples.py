b845510fa6a13f524ddf91f5bb77536a85c85a85
EmiliaGibson <87773313+EmiliaGibson@users.noreply.github.com>
2024-09-09 11:42:51 +0200

    Update examples.py

--- examples.py
+++ examples.py
@@ -1,23 +1,23 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import os
 from utils import find_surfaces
-
 
 datasets = ['Curvilinear-Sierpinski-data.npy','Henon-data.npy','Sierpinski-data.npy','Logistic-data.npy']
 
-#dictionary with algorithms parameters for each dataset
+
 dataset_kwargs = {
     'Curvilinear-Sierpinski-data.npy': {
         "delta":0.15,
-        "theta":0.3
+        "theta":1.75
     },
     'Henon-data.npy': {
-        "delta":0.1,
-        "theta":0.25
+        "delta":0.15,
+        "theta":0.15
     },
     'Sierpinski-data.npy': {
         "delta":0.1,
-        "theta":0.25
+        "theta":1.75
     },
     'Logistic-data.npy': {
         "delta":0.05,
@@ -25,29 +25,32 @@ dataset_kwargs = {
     }
 }
 
-# choose dataset 0 = curvilinear sierpisnki, 1 = henon, 2 = sierpinski, 3 = logistic  
-key = 2
+# choose dataset 0 = curvilinear sierpisnki, 1 = henon or 2 = sierpinski 
+key = 1
 assert key in [0,1,2,3]
 dataset = datasets[key]
+data = np.load('Data/'+datasets[key])
 
-#load dataset
-data = np.load('Data/' + datasets[key])
-
-#embed the data to higher dimension (here we choose the first coordinate map with the input data[1:,0])
-embedded_data = np.column_stack((data[:-1,:],data[1:,0]))
-
-#apply separation algorithm
+embedded_data = np.column_stack((data[:-1,:],data[1:,:]))
 surfaces = find_surfaces(embedded_data, **dataset_kwargs[dataset])
 
 
 #plot the different surfaces and their respective densities
-fig = plt.figure(figsize=(6,6))
-ax = plt.axes(projection='3d')
-for i in range(len(surfaces)):
-    print(len(surfaces[i]))
-    if len(surfaces[i])>0:
-        ax.plot(surfaces[i][:,0],surfaces[i][:,1],surfaces[i][:,2],'.',alpha = 0.4,label = str(i));
-plt.show()
+if key in [0,1,2]:
+    fig = plt.figure(figsize=(6,6))
+    ax = plt.axes(projection='3d')
+    for i in range(len(surfaces)):
+        print(len(surfaces[i]))
+        if len(surfaces[i])>0:
+            ax.plot(surfaces[i][:,0],surfaces[i][:,1],surfaces[i][:,2],'.',alpha = 0.4,label = str(i));
+    plt.show()
+
+elif key == 3:
+    fig = plt.figure(figsize=(6,6))
+    for i in range(len(surfaces)):
+        print(len(surfaces[i]))
+        if len(surfaces[i])>0:
+            plt.plot(surfaces[i][:,0],surfaces[i][:,1],'.',alpha = 0.4,label = str(i));
 
 
 distr = []
@@ -55,6 +58,7 @@ for i in range(len(surfaces)):
     if len(surfaces[i])>1:
         distr.append(len(surfaces[i]))
 
+fig = plt.figure(figsize=(6,6))
 plt.bar(np.arange(1,len(surfaces)+1),distr/np.sum(distr))
 plt.ylabel('density')
 plt.xlabel('$f_i$')
