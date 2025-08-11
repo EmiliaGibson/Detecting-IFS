b27ac999644ec6086e1497d8f2b43e9156e27678
EmiliaGibson <87773313+EmiliaGibson@users.noreply.github.com>
2025-08-11 11:43:18 +0200

    Update utils_mc.py

--- Utils/utils_mc.py
+++ Utils/utils_mc.py
@@ -8,7 +8,7 @@ import igraph as ig
 
 
 
-# %%
+
 def transition_matrix(data):
     '''
     code taken from https://stackoverflow.com/questions/46657221/generating-markov-transition-matrix-in-python
@@ -53,11 +53,6 @@ def get_embedded_MC(mat,delay):
     return np.array(transition_matrix(delay_chain))
 
 
-# %%
-
-
-# havent used a directed cycle basis
-
 
 def unembed_MC(Q,m):
     '''
@@ -92,13 +87,6 @@ def unembed_MC(Q,m):
 
     while (labels==None).any() and len(elementary_cycles)>0:
         P, elementary_cycles, seen_nodes,labels = examine_next_loop(Q,Q_adj,m,P,elementary_cycles,seen_nodes,labels)
-    # P, cycle_basis, seen_nodes,labels = examine_next_loop(Q,Q_adj,m,P,cycle_basis,seen_nodes,labels)
-    # P, cycle_basis, seen_nodes,labels = examine_next_loop(Q,Q_adj,m,P,cycle_basis,seen_nodes,labels)
-    # P, cycle_basis, seen_nodes,labels = examine_next_loop(Q,Q_adj,m,P,cycle_basis,seen_nodes,labels)
-    # P, cycle_basis, seen_nodes,labels = examine_next_loop(Q,Q_adj,m,P,cycle_basis,seen_nodes,labels)
-    # P, cycle_basis, seen_nodes,labels = examine_next_loop(Q,Q_adj,m,P,cycle_basis,seen_nodes,labels)
-    # P, cycle_basis, seen_nodes,labels = examine_next_loop(Q,Q_adj,m,P,cycle_basis,seen_nodes,labels)
-    # P, cycle_basis, seen_nodes,labels = examine_next_loop(Q,Q_adj,m,P,cycle_basis,seen_nodes,labels)
 
     #remove multi-edges and combine weights by taking average
     P.simplify(loops = False, combine_edges="mean")
@@ -112,27 +100,20 @@ def unembed_MC(Q,m):
 
 def examine_next_loop(Q,Q_adj,m,P,cycle_basis,seen_nodes,labels):
     loop = cycle_basis.pop(0)
-    print('loop = ',loop)
     for node in loop:
 
         #shortest path from new node to seen_nodes, d(node,L)
         path_from = min(Q.get_shortest_paths(node,seen_nodes, output="vpath",mode='out'),key=len) 
-        print(path_from)
         if len(path_from)-1 < m :
             labels = get_shared_labels(m,len(path_from)-1,labels,path_from[0],path_from[-1],direction ='out')
-            print(labels[loop])
             #prop2.5ii
             labels = rotate_loop_labels(m,loop,node,labels)
-            print(labels[loop])
 
         #shortest path from seen nodes to new node, d(L,node)
         path_to = min(Q.get_shortest_paths(node,seen_nodes, output="vpath",mode='in'),key=len)
-        print(path_to)
         if len(path_to) -1 < m :
             labels = get_shared_labels(m,len(path_to)-1,labels,path_to[0],path_to[-1], direction = 'in')
-            print(labels[loop])
             labels = rotate_loop_labels(m,loop,node,labels)
-            print(labels[loop])
 
 
     if (labels[loop]==None).any() == False:
@@ -216,176 +197,17 @@ def rotate_loop_labels(m,loop,node,labels):
 
 def get_shared_labels(m,dist, labels, unlabelled_node, labelled_node, direction):
     if direction == 'out':
-        print(labelled_node,labels[labelled_node])
 
         #From new node to seen_nodes, d(node,L)
         labels[unlabelled_node][dist:] = labels[labelled_node][:m-dist]
 
     if direction =='in':
-        print(labelled_node,labels[labelled_node])
         #From seen nodes to new node, d(L,node)
         labels[unlabelled_node][:m-dist] = labels[labelled_node][dist:]
 
     return labels
 
 
-
-
-
-
-# def transition_matrix(data):
-#     '''
-#     code adapted from https://stackoverflow.com/questions/46657221/generating-markov-transition-matrix-in-python
-#     '''
-#     n = int(max(data)) #number of states assuming states are labelled 1,..,n
-
-#     M = [[0]*n for _ in range(n)]
-
-#     for (i,j) in zip(data,data[1:]):
-#         if isinstance(i,int) and isinstance(j,int):
-#             M[i-1][j-1] += 1
-
-#     #now convert to probabilities:
-#     for row in M:
-#         s = sum(row)
-#         if s > 0:
-#             row[:] = [f/s for f in row]
-#     return np.array(M)
-
-# def get_shared_nodes(m,Q,dists,paths,nearest_loops,loop,labels):
-#     loop = deque(loop)
-#     for i in range(len(dists)):
-#         nearest_loop = deque(nearest_loops[i])
-#         idx = nearest_loop.index(paths[i][0])
-#         idx2 = loop.index(paths[i][-1])
-
-#         if dists[i]>0:
-#             loop.rotate(int(dists[i]))
-#             labels[loop[idx2]] = labels[paths[i][0]]
-#             for j in range(int(m-dists[i]-1)):
-#                 nearest_loop.rotate(1)
-#                 loop.rotate(1)
-#                 labels[loop[idx2]] = labels[nearest_loop[idx]]
-#         if dists[i]==0:
-#             intersecting_nodes = list(set(nearest_loop).intersection(loop))
-#             # for each node in the new loop that is distance one (behind) shared node in common
-#             # add the m labels shared in the original MC
-#             out = np.array([nx.multi_source_dijkstra(Q,set(nearest_loop).difference(loop),j,weight=None) for j in intersecting_nodes],dtype=object)
-#             for j in np.where(out[:,0]==1)[0]:
-#                 node = intersecting_nodes[j]
-#                 idx = nearest_loop.index(node)
-#                 idx2 = loop.index(node)
-#                 labels[loop[idx2]] = labels[nearest_loop[idx]]
-#                 for j in range(m-1):
-#                     nearest_loop.rotate(1)
-#                     loop.rotate(1)
-#                     labels[loop[idx2]] = labels[nearest_loop[idx]]
-
-#     return labels
-
-
-# def create_new_loop(Q_adj,P,loop,labels):
-#     n = len(loop)
-#     for x in loop:
-#         if labels[x] == None:
-#             labels[x] = x
-#     for i in range(n):
-#         prob = Q_adj[loop[i%n],loop[(i+1)%n]]
-#         P.add_edge(labels[loop[i%n]],labels[loop[(i+1)%n]],weight = prob)
-
-#     return P, labels
-    
-# def connect_smallest_closed_loops(Q,Q_adj,m,P,closed_loops,seen_loops,labels):
-#     #now find nearest loop to found loops
-#     while len(closed_loops)>0:
-#         #if adjacency matrix is a probability matrix we are done!
-#         if np.linalg.norm(np.sum(nx.adjacency_matrix(P).todense(),axis=-1)-np.ones(len(P.nodes)))<0.1:      
-#             return P, closed_loops, seen_loops, labels
-#         for x in closed_loops:
-#             dists,paths,nearest_loops = np.inf*np.ones(len(seen_loops)),np.zeros(len(seen_loops),dtype=object),np.zeros((len(seen_loops)),dtype=object)
-#             for j in x:
-#                 out = np.array([nx.multi_source_dijkstra(Q,set(l),j,weight=None) for l in seen_loops],dtype=object)
-#                 indices = np.where(dists-out[:,0]>0)[0]
-#                 if len(indices)>0:
-#                     dists[indices] = out[indices,0]
-#                     paths[indices] = out[indices,1]
-#                     nearest_loops[indices] = np.array(seen_loops,dtype=object)[indices].tolist()
-#             # nearest_loops = nearest_loops.reshape(paths.shape)
-            
-#             # original MC is connected so there will always exist a loop which attaches to the starting loop
-#             if np.min(dists)<m:
-
-#                 #keep cycles that are of a distance < m only
-#                 indices = np.where(dists<m)[0]
-#                 dists = dists[indices]
-#                 paths = paths[indices]
-#                 nearest_loops = nearest_loops[indices]
-
-#                 loop = x
-#                 closed_loops.remove(loop)
-
-#                 #check to see loop corresponds to a simple closed loop in P
-#                 if len([j for j in range(len(dists)) if dists[j] < m - len(nearest_loops[j])])>0:
-#                     break
-#                 # if dist_to_nearest_loop < m - len(nearest_loop):
-#                 #     break
-#                 subsets = [l for l in seen_loops if set(l).issubset(loop)==True  ]
-#                 if len(subsets)>0:
-#                     break    
-
-#                 #so loop corresponds to simple closed loop in P
-#                 seen_loops.append(loop)
-#                 # get labels of nodes in common
-#                 labels =  get_shared_nodes(m,Q,dists,paths,nearest_loops,loop,labels)
-#                 #create new loop
-#                 P,labels = create_new_loop(Q_adj,P,loop,labels)
-
-#                 #find next nearest and shortest cycle
-#                 break
-                
-#         if np.min(dists) >= m:
-#             break
-#     return P, closed_loops, seen_loops, labels
-
-# def unembed_MC(Q,m):
-#     '''
-#     Q: Network of embedded MC
-#     return P: square numpy array (original probability matrix)
-
-#     '''
-#     P = nx.DiGraph()
-#     closed_loops = list(nx.simple_cycles(Q))
-#     closed_loops.sort(key=len)
-#     #adjacency matric
-#     Q_adj = nx.adjacency_matrix(Q).todense()
-#     labels = [None for _ in list(range(Q_adj.shape[0]))]
-    
-
-#     #create first closed loop in P from Q
-#     loop = closed_loops.pop(0)
-#     n = len(loop)
-#     for i in range(n):
-#         prob = Q_adj[loop[i%n],loop[(i+1)%n]]
-#         P.add_edge(loop[i%n],loop[(i+1)%n],weight = prob)
-#         labels[loop[i]] =loop[i]
-    
-#     seen_loops = [loop]
-#     P, closed_loops, seen_loops,labels = connect_smallest_closed_loops(Q,Q_adj,m,P,closed_loops,seen_loops,labels)
-         
-
-#     return  P, labels
-
-def progressbar(i,n,refresh_rate = 1, i_start = 0):
-    '''
-    Approximates how far through the computation you are, by showing a percentage of total loops that have been computed
-    i = loop index~~~~~~~~~~~~~~~~
-    n = amount of iterations (max loop index + 1 if i_start = 0)
-    refresh_rate = how often you would like to update the percentage (useful)
-    i_start = start value of loop
-    '''
-    output = (i-i_start)/refresh_rate
-    if (int(output)==output):
-        print(str(np.round((i+1-i_start)/n*100,2))+'%', end ="\r")
 
 
 def get_original_MC(embedded_MC,labels):
@@ -398,4 +220,4 @@ def get_original_MC(embedded_MC,labels):
         if isinstance(i,int):
             original_MC.loc[embedded_MC['labels']==i] = sorted_labels.index(labels[i-1,-1])
     return original_MC
-# %%
+
